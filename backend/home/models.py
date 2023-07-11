f6451c83762dbd0a5b912196c2169918fa000d39
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-11 10:50:14 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+class Payment(models.Model):
+    'Generated Model'
+    email = models.EmailField(max_length=254,)
+    name = models.CharField(max_length=200,)
+    address = models.CharField(max_length=200,)
+    city = models.CharField(max_length=200,)
+    state = models.CharField(max_length=200,)
+    zip = models.CharField(max_length=200,)
+    country = models.CharField(max_length=200,)
